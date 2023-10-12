09f6e940f28149515f7b2a80bdad27e8a6ba658d
sally5670 <144642113+sally5670@users.noreply.github.com>
2023-10-12 15:49:14 +0900

    2

--- main.c
+++ main.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
-#include<stdlib.h>
-void print_star()
-{
-	int i;
-	for( i = 0; i < 10; i++ )
-		printf("*");
+#include <stdlib.h>
+
+void func1(void) {
+     int x;
+     printf("func1 x is at %p\n" , &x);
 }
-int main(void)
-{
-	print_star();
-	print_star();
-	print_star();
-	return 0;
+
+int main(void) {
+    int x;
+    printf("main x is at %p\n" , &x);
+    func1();
+  
+    system("PAUSE");   
+    return 0;
 }
